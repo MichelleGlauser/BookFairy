10b2608a43b4464014c284375ca1a45921bf017e
Michelle Glauser <michelleglauser@gmail.com>
2012-12-06 10:29:23 -0800

    Deploying to Heroku

--- bookapp/views.py
+++ bookapp/views.py
@@ -7,39 +7,11 @@ from django.template import RequestContext
 from django.core.urlresolvers import reverse
 from fuzzywuzzy import fuzz
 
-
-# BOOKLIST UPLOADING
-def process_book_file(book_file):
-    booklist = []
-    for line in book_file:
-        comma_index = line.rindex(",")
-        title = line[:comma_index].strip()
-        author = line[comma_index + 1:].strip()
-        booklist.append((title, author))
-    return booklist
-
-
-# LIBRARY SEARCHING
-# to retrieve the SFPL site url for any book
-def create_library_url(search_query, lib_location = "3"):
-    #PROMPT
-    #MAIN LIBRARY. THIS WILL CHANGE WHEN I ALLOW OTHER INPUT.
-    #MAKES TITLE AND AUTHOR (SEARCH QUERY) WORK IN A URL
-    joined_query = "+".join(search_query.split())
-    #SETTING UP COMPONENTS OF SEARCH URL 
-    # searchscope=library location
-    # m=media type (a=book)
-    list = ["?SEARCH=", joined_query, "&x=0&y=0&searchscope=", lib_location, "&p=&m=a&Da=&Db=&SORT=D&availlim=1"]
-    #START OF THE URL
-    library_base_url = "http://sflib1.sfpl.org/search/X"
-
-    # STICK THE PARTS TOGETHER TO CREATE A URL WITH THE SEARCH
-    for item in list:
-        library_base_url += item
-    
-    # print "LIB_URL=", library_base_url
-    return library_base_url
-
+# to retrieve book info -- is this function still doing something?
+def enter_search(request):
+    # print "Stuff"
+    form = BookForm() # An unbound form
+    return render(request, "index.html", {"form": form})
 
 def extract_details_from_single(pq_data):
     bib_info_list = pq_data("td.bibInfoData")
@@ -52,7 +24,6 @@ def extract_details_from_single(pq_data):
     author_text = author_td.text_content()
 
     return (title_text, author_text)
-
 
 def extract_details_from_multiple(book_title, author, pq_data):
     book_trs = pq_data("tr.briefCitRow")
@@ -79,7 +50,6 @@ def extract_details_from_multiple(book_title, author, pq_data):
     scored_info.sort()
     return scored_info[-1][1]
 
-
 def extract_details(book_title, author, html):
     pq_data = pq(html)
     details = extract_details_from_single(pq_data)
@@ -88,23 +58,20 @@ def extract_details(book_title, author, html):
 
     return details
 
-
 def get_library_details(book, author, library):
     url = create_library_url(book, library)
     response = requests.get(url).content
     details = extract_details(book, author, response)
     return details
 
-
-def get_details(book, author, library, library_base_url):
+def get_details(book, author, library):
     details = get_library_details(book, author, library)
     if not details:
         return None
     title = details[0]
     library_author = details[1]
-    rating = find_gr_ratings(gr_url)
+    rating = get_goodreads_rating(book)
     return (title, rating)
-
 
 # to retrieve book info -- figure out with "upload_file" function
 def check_books(request):
@@ -117,10 +84,10 @@ def check_books(request):
     lib_location = form.cleaned_data['lib_location']
     # print "%r"%lib_location 
     gr_rating = 0
-    booklist = process_book_file(book_file) # returns list of title and author
+    booklist = process_book_file(book_file) # returns booklist list
     checked_in_books = []
     for book_title, author in booklist:
-        details = get_details(book_title, author, lib_location, library_base_url)
+        details = get_details(book_title, author, lib_location)
         if details:
             checked_in_books.append(details)
             print "processed!"
@@ -134,6 +101,36 @@ def check_books(request):
         { 'booklist': checked_in_books }
         )
 
+# opens book file and puts the books in a list
+def process_book_file(book_file):
+    booklist = []
+    for line in book_file:
+        comma_index = line.rindex(",")
+        title = line[:comma_index].strip()
+        author = line[comma_index + 1:].strip()
+        booklist.append((title, author))
+    return booklist
+
+
+# to retrieve the SFPL site url for any book
+def create_library_url(search_query, lib_location = "3"):
+    #PROMPT
+    #MAIN LIBRARY. THIS WILL CHANGE WHEN I ALLOW OTHER INPUT.
+    #MAKES TITLE AND AUTHOR (SEARCH QUERY) WORK IN A URL
+    joined_query = "+".join(search_query.split())
+    #SETTING UP COMPONENTS OF SEARCH URL 
+    # searchscope=library location
+    # m=media type (a=book)
+    list = ["?SEARCH=", joined_query, "&x=0&y=0&searchscope=", lib_location, "&p=&m=a&Da=&Db=&SORT=D&availlim=1"]
+    #START OF THE URL
+    library_base_url = "http://sflib1.sfpl.org/search/X"
+
+    # STICK THE PARTS TOGETHER TO CREATE A URL WITH THE SEARCH
+    for item in list:
+        library_base_url += item
+    
+    # print "LIB_URL=", library_base_url
+    return library_base_url
 
 # to see if the book is checked in
 def check_if_in(library_base_url):
@@ -157,50 +154,22 @@ def check_if_in(library_base_url):
     return "CHECK SHELF" in availability.content 
 
 
+def get_goodreads_rating(book):
+    url = create_gr_url(book)
+    rating = find_gr_ratings(url)
+    return rating
 
-# GOODREADS SEARCHING
 # to retrieve the Goodreads site url for any book
-def create_gr_url(search_query):
-    joined_query = "+".join(search_query.split())
+def create_gr_url(gr_search_query):
+    joined_query = "+".join(gr_search_query.split())
     url_list = [joined_query, "&search_type=books"]
+    #START OF THE URL
     gr_url = "http://www.goodreads.com/search?utf8=%E2%9C%93&q="
+
+    # STICK THE PARTS TOGETHER TO CREATE A URL WITH THE SEARCH
     for item in url_list:
         gr_url += item
     return gr_url
-
-
-# to compare Goodreads authors and pick the best
-def score_gr_details(search_query):
-    gr_url = create_gr_url(search_query)
-    response = requests.get(url)
-    pq_data = pq(response.content)
-    books = pq_data("tr[itemtype='http://schema.org/Book']")
-    all_info = pq_data(books).children("td").eq(1)
-    book_info = pq_data(all_info).children("a.bookTitle")
-    author_info = pq_data(all_info).find("a.authorName").eq(0)
-    # will books here
-
-    biblio_info = []
-    for book in books: # or is it for book in book_info ?
-        # query_obj = pq(book)
-        title = pq_data(book_info).text().strip()
-        author = pq_data(author_info).text().strip()
-        if author:
-            biblio_info.append( (title, author) )
-    
-    if not biblio_info:
-        return None
-
-    scored_info = []
-    
-    for info in biblio_info:
-        title_score = fuzz.token_set_ratio(info[0], book_title)
-        author_score = fuzz.token_set_ratio(info[1], author)
-        total_score = title_score + author_score
-        scored_info.append( (total_score, info) )
-
-    scored_info.sort()
-    return scored_info[-1][1]
 
 
 # to retrieve the Goodreads rating 
@@ -217,23 +186,51 @@ def find_gr_ratings(gr_url):
     gr_rating = a.text()
     return gr_rating
 
+# to compare Goodreads authors and pick the best
+def extract_gr_titles_and_authors(gr_url):
+    # url = 
+    response = requests.get(gr_url)
+    data = pq(response.content)
+    area = data("td").eq(1)
+    a = data(area)
 
-def get_gr_details(search_query):
-    url = create_gr_url(search_query)
-    rating = find_gr_ratings(gr_url) # call this one twice?
-    scored_info = score_gr_details(search_query)
-    return rating
+    a_tags = pq_data("tr.print Row tr:first-child>td>a")
+    
+    # print a_tags
+    # filtered_tags = []
+    # for tag in a_tags:
+    #     if tag != "":
+    #         filtered_tags.append(tag)
 
-# # STEPS:
+    filtered_tags = [ tag.text for tag in a_tags if tag.text ]
+    # print filtered_tags
+    scored_tags = []
+    for tag in filtered_tags:
+        # print tag.text, book_title
+        score = fuzz.token_set_ratio(book_title, tag)
+        score_pair = (score, tag)
+        # Necessary? What if the score is below 50?
+        if score > 50: 
+            scored_tags.append(score_pair)
 
-# upload file, split into title and author
-# create a SFPL URL for the title, then request
-# check the title against the SFPL search results using fuzzywuzzy
-# check the author against the SFPL search results using fuzzywuzzy
-# add fuzzywuzzy correlation percentages and return best
-# check for availability for highest correlated items
-# create the Goodreads URL
-# check the title and author against GR search results using fuzzywuzzy
-# add fuzzywuzzy correlation percentages and return best
-# check for Goodreads rating for highest correlated item
-# put checked in books and Goodreads ratings in a list that prints on the page
+    # Other way to write it:
+    # scored_tags = [ (fuzz.token_set_ratio(title, tag), tag) 
+    #                     for tag in filtered_tags ]
+
+    scored_tags.sort()
+    if not scored_tags:
+        return None
+
+    #  
+    best = scored_tags[-1]
+    print scored_tags
+    print best
+    return best[1]
+
+# STEPS:
+
+#     get title and author from library and GR using pyquery 
+#     use fuzzywuzzy to compare to split query lines
+#     how to connect the lists?
+#     choose book with highest correlation between search and title and author
+#     put rating into the list to be put on the booklist page
