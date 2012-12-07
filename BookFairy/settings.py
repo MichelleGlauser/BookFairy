c4fe574567f514353efd190c2b57cae7c0d5d503
Michelle Glauser <michelleglauser@gmail.com>
2012-12-07 12:16:05 -0800

    root(static)

--- BookFairy/settings.py
+++ BookFairy/settings.py
@@ -66,8 +66,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/Users/michelleglauser/Desktop/BookFairy/bookapp/static'
-# STATIC_ROOT = root("static")
+#STATIC_ROOT = '/Users/michelleglauser/Desktop/BookFairy/bookapp/static'
+STATIC_ROOT = root("static")
 
 
 
