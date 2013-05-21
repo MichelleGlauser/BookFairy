8c91f2f695985fbc3d073d23a570ae3561db5f63
Michelle Glauser <michelleglauser@gmail.com>
2013-05-21 16:31:58 -0700

    Single-Sign-On and PostgreSQL

--- BookFairy/bookapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from models import Book, BookList, Bookie, AdminAccount
-
-class BookappAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Book, BookappAdmin, Bookie)
-admin.site.register(BookList, BookappAdmin, Bookie)
-
--- BookFairy/booklist.py
+++
@@ -1,7 +0,0 @@
-book_titles = ['Title,Author\rThe Hobbit,Tolkien\rA Tree Grows in Brooklyn,Betty Smith\rProgramming Interviews Exposed,Mongan\rAtlas Shrugged ,Ayn Rand\rTheir Eyes Were Watching God,Zora Neale Hurston'] 
-print book_titles
-
-def strip_booklist():
-    for book in book_titles[1:]:
-        book = book.strip
-        print book
