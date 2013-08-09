ccb94cc93314cc6cd297592d5e8da0988c81b12b
MichelleGlauser <michelleglauser@gmail.com>
2013-08-09 05:43:37 -0700

    Playing with Gunicorn, settings, index.

--- bookapp/models.py
+++ bookapp/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User, check_password
-import fastpass
+# import fastpass
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
